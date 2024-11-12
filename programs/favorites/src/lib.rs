d7600e415d6d94b16b972a7c4bd6673ae692b86a
Lilian Burdianov <lilian.burdianov@gmail.com>
2024-11-12 23:08:00 +0400

    complete the program

--- programs/favorites/src/lib.rs
+++ programs/favorites/src/lib.rs
@@ -8,14 +8,32 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub mod favorites {
     use super::*;
 
-    pub fn set_favorites(ctx: Context<Initialize>) -> Result<()> {
+    pub fn set_favorites(
+        ctx: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let user_public_key = ctx.accounts.user.key();
+
+        msg!(
+            "User {:?}, favorite number: {:?}, color: {:?}, hobbies: {:?}",
+            user_public_key,
+            number,
+            color,
+            hobbies
+        );
+
+        ctx.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
 
 #[account]
 #[derive(InitSpace)]
@@ -25,4 +43,20 @@ pub struct Favorites {
     pub color: String,
     #[max_len(5, 50)]
     pub hobbies: Vec<String>,
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
 }
